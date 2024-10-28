fa1e58b9bf705e68b90f032cb098e7d3663e07ab
V P <vpernicka@gmail.com>
2024-10-28 17:03:20 +0100

    Add simple bitcoin rate download and display

---
+++ BitcoinRateWeb/Models/SimpleCurrencyConvertor.cs
@@ -0,0 +1,34 @@
+﻿namespace BitcoinRateWeb.Models
+{
+    public class SimpleCurrencyConvertor : ICurrencyConvertor
+    {
+        public string FromCurrencyCode { get; }
+        public string ToCurrencyCode { get; }
+        public decimal Rate { get; }
+
+        public SimpleCurrencyConvertor(string fromCurrencyCode, string toCurrencyCode, decimal rate)
+        {
+            FromCurrencyCode = fromCurrencyCode;
+            ToCurrencyCode = toCurrencyCode;
+            Rate = rate;
+        }
+
+        public bool CanConvert(string fromCurrencyCode, string toCurrencyCode)
+        {
+            if (fromCurrencyCode != FromCurrencyCode) return false;
+            if (toCurrencyCode != ToCurrencyCode) return false;
+
+            return true;
+        }
+
+        public decimal Convert(decimal value, string fromCurrencyCode, string toCurrencyCode)
+        {
+            if (!CanConvert(fromCurrencyCode, toCurrencyCode))
+            {
+                throw new NotSupportedException();
+            }
+
+            return value * Rate;
+        }
+    }
+}
