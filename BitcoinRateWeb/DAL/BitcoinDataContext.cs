d8737e017f902942e917688fca7bb15ebe2fe245
V P <vpernicka@gmail.com>
2024-10-28 22:56:58 +0100

    Add Saved Data page with adding and deleting

---
+++ BitcoinRateWeb/DAL/BitcoinDataContext.cs
@@ -0,0 +1,15 @@
+﻿using BitcoinRateWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BitcoinRateWeb.DAL
+{
+    public class BitcoinDataContext : DbContext
+    {
+        public BitcoinDataContext(DbContextOptions<BitcoinDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BitcoinPrice> BitcoinPrices { get; set; }
+    }
+}
---
+++ BitcoinRateWeb/Models/BitcoinPrice.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BitcoinRateWeb.Models
+{
+    public class BitcoinPrice
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+        public decimal BtcToEur { get; set; }
+        public decimal BtcToCzk { get; set; }
+
+        [MaxLength(100)]
+        public string Note { get; set; }
+    }
+}
